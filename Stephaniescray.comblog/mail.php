101eddf3de2a8dc22cda1c5b8565a915570e135b
Stephanie Cray <stephanie.cray@yahoo.com>
2018-10-07 13:45:18 -0400

    adding in website

---
+++ Stephaniescray.comblog/mail.php
@@ -0,0 +1,12 @@
+
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "stephanie.cray@yahoo.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! I will contact you soon." ." -" . "<a href='index.html> Returning Home!'</a>";
+?>
